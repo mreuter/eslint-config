0b137dfd092ee16effe0a53f137185293859dd75
Marc Reuter <marc@twisted-pair.de>
2018-10-02 12:35:50 +0200

    Added .editorconfig

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-        node: true
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:compat/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      generators: true,
     },
-    extends: [
-        "airbnb",
-        "plugin:flowtype/recommended",
-        "plugin:react/recommended",
-        "plugin:import/recommended",
-        "plugin:compat/recommended"
+  },
+  'plugins': ['prefer-object-spread', 'flowtype', 'react', 'import', 'compat'],
+  'rules': {
+    'react/jsx-filename-extension': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {'devDependencies': true, 'optionalDependencies': false, 'peerDependencies': true},
     ],
-    parserOptions: {
-        ecmaVersion: 2017,
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true,
-            generators: true
-        }
-    },
-    "plugins": ["prefer-object-spread", "flowtype", "react", "import", "compat"],
-    "rules": {
-        "react/jsx-filename-extension": "off",
-        "import/no-extraneous-dependencies": [
-            "error",
-            { "devDependencies": true, "optionalDependencies": false, "peerDependencies": true }
-        ]
-    }
+  },
 };
